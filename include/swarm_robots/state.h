2744ce38b42da5b55ba3bb179cb7fdab490777d7
Kavyashree <kavya27.devadiga@gmail.com>
2021-12-12 02:32:43 -0500

    Task 3.01
    Add methods in robot stte class to create and publish twist messages on robot cmd_vel topic

--- include/swarm_robots/state.h
+++ include/swarm_robots/state.h
@@ -12,15 +12,25 @@
 #ifndef INCLUDE_SWARM_ROBOTS_STATE_H_
 #define INCLUDE_SWARM_ROBOTS_STATE_H_
 
-class State {
- public:
-  State();
-  State(double x, double y);
-  State(double x, double y, double yaw);
-  double x_;
-  double y_;
-  double yaw_;
-};
+#include <iostream>
+#include <string>
 
+#include "geometry_msgs/Pose.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "tf/transform_broadcaster.h"
+
+class State {
+   public:
+    State();
+    State(double x, double y);
+    State(double x, double y, double yaw);
+    double x_;
+    double y_;
+    double yaw_;
+    ros::Publisher state_pub;
+    bool SetPublisher(ros::NodeHandle nh, std::string topic);
+    bool Publish(geometry_msgs::Twist);
+};
 
 #endif  // INCLUDE_SWARM_ROBOTS_STATE_H_
--- src/state.cpp
+++ src/state.cpp
@@ -1,20 +1,47 @@
-#include "state.h"
+/**
+ * @file state.cpp
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam
+ * (aswath@umd.edu)
+ * @brief The header file for defining robot state variables and methods
+ * @version 0.2
+ * @date 2021-12-11
+ * @copyright BSD3 Copyright (c) 2021
+ *
+ */
+#include "../include/swarm_robots/state.h"
 
+#include <iostream>
+#include <string>
 
-State::State(){
-    this->x_=0;
-    this->y_=0;
-    this->yaw_=0;
+#include "geometry_msgs/Pose.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "tf/transform_broadcaster.h"
+
+State::State() {
+    this->x_ = 0;
+    this->y_ = 0;
+    this->yaw_ = 0;
 }
 
 State::State(double x, double y) {
     this->x_ = x;
     this->y_ = y;
     this->yaw_ = 0;
-} 
+}
 
-State::State(double x, double y, double yaw){
+State::State(double x, double y, double yaw) {
     this->x_ = x;
     this->y_ = y;
     this->yaw_ = yaw;
+}
+
+bool State::SetPublisher(ros::NodeHandle nh, std::string topic) {
+    state_pub = nh.advertise<geometry_msgs::Twist>(topic, 1);
+    return true;
+}
+
+bool State::Publish(geometry_msgs::Twist robot_vel) {
+    state_pub.publish(robot_vel);
+    return true;
 }
